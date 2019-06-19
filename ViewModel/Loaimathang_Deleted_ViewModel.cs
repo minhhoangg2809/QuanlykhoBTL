521e263e4570f5ca178628c80b025fd154b1b0c7
minhhoang2809 <minhhoangj1998@gmail.com>
2019-06-19 23:28:36 +0700

    ViewModel

--- ViewModel/Loaimathang_Deleted_ViewModel.cs
+++ ViewModel/Loaimathang_Deleted_ViewModel.cs
@@ -47,7 +47,6 @@ namespace QLK_Dn.ViewModel
         public ICommand RestoreShow_Command { get; set; }
         public ICommand DelShow_Command { get; set; }
         public ICommand ResShow_Command { get; set; }
-        public ICommand Search_Command { get; set; }
         public ICommand Sort_Command { get; set; }
 
         #endregion
@@ -252,44 +251,6 @@ namespace QLK_Dn.ViewModel
                 RDList = new ObservableCollection<Model.LOAIHANG>();
                 Opendel = false;
             });
-            #endregion
-
-            #region Phan tim kiem
-
-            Search_Command = new RelayCommand<TextBox>(p =>
-            {
-                return true;
-            }, p =>
-            {
-                string str = p.Text;
-                List = new ObservableCollection<Model.LOAIHANG>(Model.DataProvider.Ins.DB.LOAIHANGs.Where(x => x.IsDeleted == true));
-
-                if (!string.IsNullOrEmpty(str))
-                {
-                    var filterlist = List.Where(x => x.ten_loaihang.Contains(str));
-
-                    for (int i = 0; i < List.Count(); i++)
-                    {
-                        while (!filterlist.Contains(List[i]))
-                        {
-                            if (List[i] == List[List.Count() - 1])
-                            {
-                                List.Remove(List[i]);
-                                break;
-                            }
-                            else
-                            {
-                                List.Remove(List[i]);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    List = new ObservableCollection<Model.LOAIHANG>(Model.DataProvider.Ins.DB.LOAIHANGs.Where(x => x.IsDeleted == true));
-                }
-            });
-
             #endregion
 
             #region Phan sap xep
