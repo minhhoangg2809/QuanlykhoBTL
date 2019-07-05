c7dcc88b07b4603658910130e8b971a51b183676
minhhoang2809 <minhhoangj1998@gmail.com>
2019-07-05 23:04:13 +0700

    My Source

--- MySource/MyStaticMethods.cs
+++ MySource/MyStaticMethods.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace QLK_Dn.MySource
 {
@@ -102,6 +103,34 @@ namespace QLK_Dn.MySource
         {
             DateTime Myreal_Date = MyStaticMethods.ConvertStringtoDate(str);
             return (Myreal_Date.Month.ToString() + "/" + Myreal_Date.Day.ToString() + "/" + Myreal_Date.Year.ToString());
+        }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+
+                {
+
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+
+                    if (child != null && child is T)
+
+                    {
+
+                        yield return (T)child;
+
+                    }
+
+
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
         }
     }
 }
